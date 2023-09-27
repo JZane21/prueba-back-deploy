3f53f671a24332841cacdb673cf5ba895b3e2245
Jose Carrasco <jsce2021@gmail.com>
2023-09-26 20:18:20 -0400

    new commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,5 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable, Logger } from '@nestjs/common';
-import { AxiosError, AxiosResponse } from 'axios';
-import { Observable, catchError, firstValueFrom } from 'rxjs';
+import { Injectable } from '@nestjs/common';
 
 export interface MessageInterface{
   message:string;
@@ -9,20 +7,20 @@ export interface MessageInterface{
 
 @Injectable()
 export class AppService {
-  private readonly logger = new Logger(AppService.name);
+  // private readonly logger = new Logger(AppService.name);
   constructor(private readonly httpService: HttpService) {}
 
-  async findAll(): Promise<AxiosResponse<string>> {
-    const { data } = await firstValueFrom(
-      this.httpService.get<AxiosResponse<string>>('http://localhost:5000').pipe(
-        catchError((error: AxiosError) => {
-          this.logger.error(error.response.data);
-          throw 'An error happened!';
-        }),
-      ),
-    );
-    return data;
-  }
+  // async findAll(): Promise<AxiosResponse<string>> {
+  //   const { data } = await firstValueFrom(
+  //     this.httpService.get<AxiosResponse<string>>('http://localhost:5000').pipe(
+  //       catchError((error: AxiosError) => {
+  //         this.logger.error(error.response.data);
+  //         throw 'An error happened!';
+  //       }),
+  //     ),
+  //   );
+  //   return data;
+  // }
 
   getHello(): MessageInterface {
     return {message:"message"};
